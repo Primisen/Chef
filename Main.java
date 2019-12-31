34bd18cb301a18754c5b187aa7514de388dc8df1
Nadia Shokel <primisen17@gmail.com>
2019-12-31 19:56:27 +0300

    Update and rename Application.java to Main.java

--- Main.java
+++ Main.java
@@ -9,30 +9,16 @@ import com.primisen.java_november.part4.util.RecipeParser;
 
 import java.util.ArrayList;
 
-/*
-Шеф-повар.
-- Определить иерархию овощей (плодовые, корнеплодные и листовые).
-+ Сделать салат.
-+ Посчитать калорийность.
-+ Провести сортировку овощей для салата на основе одного
-+ и нескольких параметров.
-+ Найти ингредиенты в салате, соответствующие заданному диапазону калорийности.
- */
-public class Application {
-
-    private static Dialog dialog;
-
 
     public static void main(String[] args) {
 
         FileReader fileReader = new FileReader("monomah.txt", "D:\\Java\\training november\\");
         RecipeParser recipeParser = new RecipeParser(fileReader.read());
 
-        recipeParser.findIngredient();
-
         ArrayList<Ingredient> ingredients = new ArrayList<>();
 
-        Salad salad = new Salad();
+        ingredients = recipeParser.findIngredient();
 
+        Salad salad = new Salad(ingredients);
     }
 }
