d62fa74582ec6bf6d90cf1e3673f1c155a08f72f
Nadia Shokel <primisen17@gmail.com>
2020-01-10 18:15:47 +0300

    Update Chef.java

--- chef/Chef.java
+++ chef/Chef.java
@@ -1,16 +1,21 @@
 package com.primisen.java_november.part4.chef;
 
 import com.primisen.java_november.part4.ingredient.Ingredient;
-import com.primisen.java_november.part4.ingredient.vegetable.fruit.Fruit;
-import com.primisen.java_november.part4.ingredient.vegetable.leafy.Leafy;
+import com.primisen.java_november.part4.utility.RecipeReader;
+import com.primisen.java_november.part4.utility.RecipeReaderLogic;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Chef {
 
     private ArrayList<Ingredient> ingredients;
 
-    public Chef(){}
+    public Chef(String recipe) throws IOException {
+        RecipeReader recipeReader = new RecipeReader(recipe);
+        RecipeReaderLogic recipeReaderLogic = new RecipeReaderLogic(recipeReader);
+        this.ingredients = recipeReaderLogic.readRecipe();
+    }
 
     public Chef(ArrayList<Ingredient> ingredients){
         this.ingredients = ingredients;
